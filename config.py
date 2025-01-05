428a291e358e1aac1369cd81f2899237ed00ae76
Colleen Banzhof <colleen_huff@yahoo.com>
2025-01-05 15:50:32 -0600

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ cleaned_csv = "US_Accidents_Cleaned.csv"
 # output zip path
 destination_zip = os.path.join("Resources", "US_Accidents.zip")
 
-# column names and data types
+# column names and data type
 col_types = {'ID': 'str', 
              'Severity': 'int8', 
              'Start_Time': 'str'}
